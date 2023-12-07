4880b365fb347d6f5606567581296bfc9e8335a0
Kieran Livesey <kieran@swirrl.com>
2023-12-07 16:32:36 +0000

    Do some interactivity and use blob client

---
+++ public/map.js
@@ -0,0 +1,79 @@
+
+
+
+var width = 800;
+var height = 800;
+
+svg = d3.select("#the-map")
+    .append('svg')
+    .attr('preserveAspectRatio', 'xMaxYMax meet')
+    .attr("width", width)
+    .attr("height", height)
+    .attr('viewBox', '0 0 ' + width + ' ' + height);
+
+function dataSource() {
+    let set = d3.select('#dataSet').property("value");
+    let year = d3.select('#dataYear').property("value");
+    return 'data/' + set + 'LAD' + year + '.csv';
+}
+
+function ready(error, geojson, datarows) {
+    // D3 expects polygon winding contrary to the GeoJSON right-hand rule,
+    // so we need to rewind any polygons in the feature collection
+    var rewoundFeatures = geojson.features.map(function (feature) {
+        return turf.rewind(feature, {reverse: true});
+    });
+    geojson.features = rewoundFeatures;
+    let data = Object.fromEntries(datarows);
+
+    let selectedScheme = d3.select('#colourSelector').property("value");
+
+    // find min and max the ol' fashioned way
+    let min = Infinity, max = -Infinity;
+    for (let v of Object.values(data)) {
+        min = Math.min(v, min);
+        max = Math.max(v, max);
+    }
+
+    function gradient(v) {
+        let linear = (v - min) / (max - min); // v mapped to 0..1 linearly
+        // do logarithmic/pow/sqrt/etc scaling here!
+        // alternatively do some binning or whatever
+        return d3[selectedScheme](linear);
+    }
+
+    var projection = d3.geoMercator()
+        .fitSize([width, height], geojson);
+
+    var path = d3.geoPath().projection(projection);
+
+    // Draw the map
+    svg.selectAll('*').remove();
+    svg.append("g")
+        .selectAll("path")
+        .data(geojson.features)
+        .enter().append("path")
+        .attr("d", path)
+        .attr("fill", function (d) {
+            var geocode = d.properties["LAD23CD"];
+            var value = data[geocode] || 0;
+            return gradient(value);
+        })
+        .style("stroke", "#000")
+        .style("stroke-width", 0.5)
+        .style('stroke-linejoin', 'miter');
+
+}
+
+function render() {
+
+    d3.queue()
+        .defer(d3.json, "mapshaped-districts.geojson")
+        .defer(d3.csv, dataSource(), (d) => [d.GEOGRAPHY_CODE, +d.OBS_VALUE])
+        .await(ready);
+
+}
+render();
+
+d3.selectAll('.map-rerenderer').attr('onchange', 'render()');
+
--- src/index.mjs
+++ src/index.mjs
@@ -1,11 +1,24 @@
 
+import blobs from '@azure/storage-blob';
+
 import express from 'express';
 
+// express < 5 can't handle async exceptions in handlers
+import 'express-async-errors';
+
 const port = process.env.PORT || 8000;
+
+const blobService = blobs.BlobServiceClient.fromConnectionString(process.env.MAPDEMO_BLOB_CONNECTION);
+const blobContainer = blobService.getContainerClient(process.env.MAPDEMO_BLOB_CONTAINER);
 
 const app = express();
 
 app.use(express.static('public'));
+
+app.get('/data/:file', async (req, res) => {
+    let blobClient = blobContainer.getBlockBlobClient(`${process.env.MAPDEMO_BLOB_DIR}/${req.params.file}`);
+    (await blobClient.download()).readableStreamBody.pipe(res);
+});
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
